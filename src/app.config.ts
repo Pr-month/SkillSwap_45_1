9950ee77e3cdf5373448de4b88d7dac8c4503c01
kater-g <ekaternag.garipova@yandex.ru>
2026-07-01 13:59:01 +0500

    feat(config): добавила app.config и значения переменных в env.example

---
+++ src/app.config.ts
@@ -0,0 +1,8 @@
+import { registerAs, ConfigType } from '@nestjs/config';
+
+export const appConfig = registerAs('APP_CONFIG', () => ({
+  port: Number(process.env.PORT) || 3000,
+  hashSalt: Number(process.env.HASH_SALT) || 10,
+}));
+
+export type IAppConfig = ConfigType<typeof appConfig>;
