ca46e46ecbfa49b4f7a9fcf116b3e30f7602b0e0
kater-g <ekaternag.garipova@yandex.ru>
2026-07-01 23:39:37 +0500

    fix(config): добавила библиотеку ms, типизацию для jwt, использовала порт из конфига в main.ts

--- src/jwt.config.ts
+++ src/jwt.config.ts
@@ -1,10 +1,11 @@
 import { registerAs, ConfigType } from '@nestjs/config';
+import ms from 'ms';
 
 export const jwtConfig = registerAs('JWT_CONFIG', () => ({
   accessSecret: process.env.JWT_ACCESS_SECRET || 'jwt-access-secret',
   refreshSecret: process.env.JWT_REFRESH_SECRET || 'jwt-refresh-secret',
-  accessExpiresIn: process.env.JWT_ACCESS_EXPIRES_IN || '1h',
-  refreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN || '7d',
+  accessExpiresIn: (process.env.JWT_ACCESS_EXPIRES_IN || '1h') as ms.StringValue,
+  refreshExpiresIn: (process.env.JWT_REFRESH_EXPIRES_IN || '7d') as ms.StringValue,
 }));
 
 export type IJwtConfig = ConfigType<typeof jwtConfig>;
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { appConfig, IAppConfig } from './app.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const config = app.get<IAppConfig>(appConfig.KEY);
+  await app.listen(config.port);
 }
 bootstrap();
