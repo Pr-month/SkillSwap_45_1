2cba9c70c10b1170bd0b8b00a1aa2e5ab39da96e
Petor Cheprasov <petor.tcheprasov@yandex.ru>
2026-07-02 22:17:47 +0700

    Задача № 23 перенос конфигов в отдельную папку

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { appConfig } from './app.config';
-import { jwtConfig } from './jwt.config';
+import { appConfig } from './config/app.config';
+import { jwtConfig } from './config/jwt.config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
@@ -13,8 +13,9 @@ import { AuthModule } from './auth/auth.module';
       isGlobal: true,
       load: [appConfig, jwtConfig],
     }),
-    UsersModule, 
-    AuthModule],
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
