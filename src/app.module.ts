9c7578361842099d22d243230f5ab3a3c039a5a0
katwhite <quataridevinni@gmail.com>
2026-07-03 09:08:24 +0300

    feat: добавила db config и подключила в app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,33 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { appConfig } from './app.config';
 import { jwtConfig } from './jwt.config';
+import { dbConfig } from './db.config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { AppDataSource } from '../ormconfig';
+// import { AppDataSource } from '../ormconfig';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSourceOptions } from 'typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [appConfig, jwtConfig],
+      load: [appConfig, jwtConfig, dbConfig],
     }),
-    TypeOrmModule.forRoot(AppDataSource.options),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const config = configService.get<DataSourceOptions>('database');
+        if (!config) {
+          throw new Error('Database configuration not found');
+        }
+        return config;
+      },
+    }),
     UsersModule, 
     AuthModule],
   controllers: [AppController],
