c9ca30ed3b93e875199605b3a2f720b601cf4dfe
Roland <machodin@yandex.ru>
2026-07-04 23:47:10 +0300

    fix errors after merge

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { appConfig } from './app.config';
-import { jwtConfig } from './jwt.config';
 import { dbConfig, TDatabaseConfig } from './config/db.config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { appConfig } from './config/app.config';
+import { jwtConfig } from './config/jwt.config';
 
 @Module({
   imports: [
