c18749683a0f3923b27773d306d9b5698e1888eb
Petor Cheprasov <petor.tcheprasov@yandex.ru>
2026-07-05 13:53:20 +0700

    Задача № 14 Получение списка пользователей по эндпоинту GET users

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { UserEntity } from './entities/user.entity';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -8,7 +10,13 @@ describe('UsersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(UserEntity),
+          useValue: { find: jest.fn() },
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,15 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UserEntity } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly usersRepository: Repository<UserEntity>,
+  ) {}
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.usersRepository.find({
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        about: true,
+        birthdate: true,
+        city: true,
+        gender: true,
+        avatar: true,
+        role: true,
+      },
+    });
   }
 
   findOne(id: number) {
