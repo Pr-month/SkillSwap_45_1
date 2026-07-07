60c4ac856a89d2dad86709399348b72e58a6d82a
kater-g <ekaternag.garipova@yandex.ru>
2026-07-07 20:09:20 +0500

    refactor(config/db): убрала дублирования подключения к БД в ormconfig.ts

--- src/config/ormconfig.ts
+++ src/config/ormconfig.ts
@@ -1,19 +1,4 @@
 import { DataSource } from 'typeorm';
-import * as dotenv from 'dotenv';
-import path from 'path';
+import { dbConfig } from './db.config';
 
-dotenv.config();
-
-export const AppDataSource = new DataSource({
-  type: process.env.DB_TYPE as 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-
-  entities: [path.join(__dirname, '..', '**', '*.entity{.ts}')],
-
-  synchronize: process.env.TYPEORM_SYNCHRONIZE === 'true',
-  logging: process.env.TYPEORM_LOGGING === 'true',
-});
+export const AppDataSource = new DataSource(dbConfig());
