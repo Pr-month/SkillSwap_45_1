b54493f218b8d28cea6382a4f735f546647b9949
kater-g <ekaternag.garipova@yandex.ru>
2026-07-07 19:47:07 +0500

    feat(users): добавила получение данных текущего пользователя по эндпоинту get(users/me)

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,10 +6,15 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
+  Request,
 } from '@nestjs/common';
+import { Request } from 'express';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { JwtPayload } from '../auth/auth.types';
 
 @Controller('users')
 export class UsersController {
@@ -23,6 +28,13 @@ export class UsersController {
   @Get()
   findAll() {
     return this.usersService.findAll();
+  }
+
+  @Get('me')
+  @UseGuards(JwtAuthGuard)
+  getCurrentUser(@Request() req: Request & { user: JwtPayload }) {
+    const userId = req.user.sub;
+    return this.usersService.findById(userId);
   }
 
   @Get(':id')
