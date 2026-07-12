26f06e78b77398c2fb3869e1eb98914ac0d5dcea
Petor Cheprasov <petor.tcheprasov@yandex.ru>
2026-07-12 18:11:16 +0700

    Задача № 45 гарда и декоратор для защиты эндпоинта по ролям версия 1.1.1

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -8,6 +8,7 @@ import { UserEntity } from '../users/entities/user.entity';
 import { UsersModule } from '../users/users.module';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { RolesGuard } from './guards/roles.guard';
 
 @Module({
   imports: [
@@ -24,7 +25,7 @@ import { JwtAuthGuard } from './guards/jwt-auth.guard';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy, JwtAuthGuard],
-  exports: [JwtModule, JwtStrategy, JwtAuthGuard],
+  providers: [AuthService, JwtStrategy, JwtAuthGuard, RolesGuard],
+  exports: [JwtModule, JwtStrategy, JwtAuthGuard, RolesGuard],
 })
 export class AuthModule {}
