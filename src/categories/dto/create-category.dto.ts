23a9e5cecf76b1f0a933c09b65621fe24a61236f
veraulitinaYa <veraulitina@yandex.ru>
2026-07-16 13:33:13 +0300

    feat: category entity

--- src/categories/dto/create-category.dto.ts
+++ src/categories/dto/create-category.dto.ts
@@ -1,1 +1,16 @@
-export class CreateCategoryDto {}
+import {
+  IsOptional,
+  IsString,
+  IsUUID,
+  MaxLength,
+} from 'class-validator';
+
+export class CreateCategoryDto {
+  @IsString()
+  @MaxLength(100)
+  name!: string;
+
+  @IsOptional()
+  @IsUUID()
+  parentId?: string;
+}
