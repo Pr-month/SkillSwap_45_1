be25b086085cf98e9a6b9c661ba430a23f7487e6
veraulitinaYa <veraulitina@yandex.ru>
2026-07-25 20:04:08 +0300

    docs: swagger auth

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString, MinLength, MaxLength } from 'class-validator';
 
 export class LoginDto {
+  @ApiProperty({ description: 'User email' })
   @IsEmail()
   email!: string;
 
+  @ApiProperty({ description: 'User password' })
   @IsString()
   @MinLength(6)
   @MaxLength(32)
